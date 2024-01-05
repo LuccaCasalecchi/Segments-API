e9f56f2e14d6bd4edda84347da83c9d0ffc3ae8e
Lucca Casalecchi <luccachermont@gmail.com>
2024-01-04 22:23:19 -0300

    tags and segments controller implementation

---
+++ app/controllers/TagsController.rb
@@ -0,0 +1,53 @@
+class TagsController < ApplicationController
+
+  # GET /tags
+  def index
+    @tags = Tag.all
+    render json: @tags
+  end
+
+  # GET /tags/:id
+  def show
+    set_tag
+    render json: @tag
+  end
+
+  # POST /tags
+  def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      render json: @tag, status: :created, location: @tag
+    else
+      render json: @tag.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /tags/:id
+  def update
+    set_tag
+    if @tag.update(tag_params)
+      render json: @tag
+    else
+      render json: @tag.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /tags/:id
+  def destroy
+    set_tag
+    @tag.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_tag
+    @tag = Tag.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Tag not found' }, status: :not_found
+  end
+
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
+end
